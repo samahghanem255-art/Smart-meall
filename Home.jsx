377b3f191ea30359afdda0f899c057c4512f7283
samahghanem255-art <samahghanem255@gmail.com>
2026-05-04 10:14:10 +0100

    Create Home.jsx

---
+++ Home.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../components/Navbar";
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <div className="container">
+        <h1>Bienvenue 🍽️</h1>
+        <p>أفضل تطبيق لطلب الطعام</p>
+      </div>
+    </>
+  );
+}
